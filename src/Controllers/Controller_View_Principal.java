d23410251b4290a90920764b8e7d324e999d1dc6
Jesús Correa <jdplayzone@gmail.com>
2024-11-21 06:13:17 -0500

    Creación de controlador para la vista principal(catálogo).

---
+++ src/Controllers/Controller_View_Principal.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Controller_View_Principal implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
