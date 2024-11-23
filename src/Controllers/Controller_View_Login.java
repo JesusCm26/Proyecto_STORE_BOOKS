260f888961dc6744dd890ead60f315e6f277e307
Miguel Macia <miguel312108@gmail.com>
2024-11-23 05:06:08 -0500

    Nombrando botones, campos y actualizando el controlador(View_Login).

--- src/Controllers/Controller_View_Login.java
+++ src/Controllers/Controller_View_Login.java
@@ -2,9 +2,22 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller_View_Login implements Initializable {
+
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private PasswordField txtPass;
+    @FXML
+    private Button btn_ingresar;
+    @FXML
+    private Button btn_registrarse;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
