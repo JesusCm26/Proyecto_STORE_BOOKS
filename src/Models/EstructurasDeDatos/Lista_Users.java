584439db5001956b0333f0c3b6695db863c32a35
Miguel Hernandez <miguelangelhernandezpadilla20@gmail.com>
2024-11-23 03:25:57 -0500

    Creando metodo capaz construir(crear) un usuario nuevo(Lista_User).

--- src/Models/EstructurasDeDatos/Lista_Users.java
+++ src/Models/EstructurasDeDatos/Lista_Users.java
@@ -1,9 +1,13 @@
 package Models.EstructurasDeDatos;
 
 import Models.Nodos.Nodo_User;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Lista_Users {
 
@@ -92,4 +96,43 @@ public class Lista_Users {
 
         return todos;
     }
+    
+    public Nodo_User crearUsuario(TextField txtNombre, TextField txtIdentificacion, TextField txtCell, TextField txtGmail, PasswordField txtPassword) {
+
+        Nodo_User buscar = buscarEmail(txtGmail.getText());
+        Nodo_User buscar2 = buscarIdentificacion(Integer.parseInt(txtIdentificacion.getText()));
+        try {
+
+            if (buscar != null) {
+                Alert(Alert.AlertType.WARNING, "Importante..!",
+                        "Ya existe un usuario con este Correo");
+                txtGmail.setText("");
+                txtGmail.requestFocus();
+                return null;
+            } else if (buscar2 != null) {
+                Alert(Alert.AlertType.WARNING, "Importante..!",
+                        "Ya existe un usuario con esta identificación");
+                txtIdentificacion.setText("");
+                txtIdentificacion.requestFocus();
+                return null;
+            } else {
+                Nodo_User nuevoU = new Nodo_User(txtNombre.getText(), Integer.parseInt(txtIdentificacion.getText()), txtCell.getText(), txtGmail.getText(), txtPassword.getText());
+                Alert(Alert.AlertType.CONFIRMATION, "Dialogo de confirmación.",
+                        "Registro realizado con exito...!\n"
+                        + "Felicidades...! ya haces parte de nuestros usuarios.");
+                txtNombre.setText("");
+                txtIdentificacion.setText("");
+                txtCell.setText("");
+                txtGmail.setText("");
+                txtPassword.setText("");
+                return nuevoU;
+            }
+
+        } catch (NumberFormatException e) {
+            Logger.getLogger(Lista_Users.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
+
+    }
+
 }
