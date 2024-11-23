5081c8bf5499d3081cdeb69bb7454e4bb750876f
Miguel Macia <miguel312108@gmail.com>
2024-11-23 05:37:44 -0500

    Agregando metodo para acción de cierre de ventana "closeWindow()" (Controller_View_Principal - Controller_View_Sing_in).

--- src/Controllers/Controller_View_Principal.java
+++ src/Controllers/Controller_View_Principal.java
@@ -1,12 +1,19 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Controller_View_Principal implements Initializable {
 
@@ -31,4 +38,22 @@ public class Controller_View_Principal implements Initializable {
     private void eventAction(ActionEvent event) {
     }
     
+    public void closeWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_Login.fxml"));
+
+            Parent roott = loader.load();
+
+            Scene scene = new Scene(roott);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+
+            Stage miStage = (Stage) this.btn_carrito.getScene().getWindow();
+            miStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller_View_Principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/Controllers/Controller_View_Sing_in.java
+++ src/Controllers/Controller_View_Sing_in.java
@@ -1,13 +1,20 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller_View_Sing_in implements Initializable {
 
@@ -35,4 +42,22 @@ public class Controller_View_Sing_in implements Initializable {
     private void eventAction(ActionEvent event) {
     }
     
+    public void closeWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_Login.fxml"));
+
+            Parent roott = loader.load();
+
+            Scene scene = new Scene(roott);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+
+            Stage miStage = (Stage) this.btn_registrarse.getScene().getWindow();
+            miStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller_View_Sing_in.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
