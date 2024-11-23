89985bb5e8d07bb5d3cc86da68c5f4abd8749324
Miguel Macia <miguel312108@gmail.com>
2024-11-23 05:25:53 -0500

    Nombrando botones, txt de identificación de usuario y agregando metodo gestor de acciones(View_Principal).

--- src/Controllers/Controller_View_Principal.java
+++ src/Controllers/Controller_View_Principal.java
@@ -2,9 +2,22 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class Controller_View_Principal implements Initializable {
+
+    @FXML
+    private Button btn_favoritos;
+    @FXML
+    private Button btn_userOptions;
+    @FXML
+    private Button btn_carrito;
+    @FXML
+    private Text txt_user;
 
     /**
      * Initializes the controller class.
@@ -13,5 +26,9 @@ public class Controller_View_Principal implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void eventAction(ActionEvent event) {
+    }
     
 }
