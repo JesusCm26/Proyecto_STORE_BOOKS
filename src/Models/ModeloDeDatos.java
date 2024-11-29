3e454102554c1be6b64cc23191a76a4fe9966086
Flintern <jesusymeri14@hotmail.com>
2024-11-29 13:31:06 -0500

    Agregando instancia de la pila de libros al gestor de instancias(ModeloDeDatos).

--- src/Models/ModeloDeDatos.java
+++ src/Models/ModeloDeDatos.java
@@ -1,14 +1,17 @@
 package Models;
 
 import Models.EstructurasDeDatos.Lista_Users;
+import Models.EstructurasDeDatos.Pila_Books;
 
 public class ModeloDeDatos {
 
     private static ModeloDeDatos instancia;
     private final Lista_Users listaU;
+    private final Pila_Books pialB;
 
     private ModeloDeDatos() {
         this.listaU = new Lista_Users();
+        this.pialB = new Pila_Books();
     }
 
     public static ModeloDeDatos obtenerInstancia() {
@@ -22,5 +25,9 @@ public class ModeloDeDatos {
     public Lista_Users getListaU() {
         return listaU;
     }
-        
+
+    public Pila_Books getPialB() {
+        return pialB;
+    }
+            
 }
